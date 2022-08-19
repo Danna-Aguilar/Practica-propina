b641bbc00b398c927889c8dabc41f1676c8deafe
Alumno <ulsa@D105.local>
2022-08-19 08:53:27 -0700

    practica

--- Practica-cuenta/ViewController.swift
+++ Practica-cuenta/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Cantidad: UITextField!
+    @IBOutlet weak var propina: UISlider!
+    @IBOutlet weak var lblContador: UILabel!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func IbSlider(_ sender: Any) {
+        let cuenta = "54.8"
+        let valorslider = Float(cuenta)
+        lblContador.text = "\(Int(propina.value))%"
+        
+        let porcentajeP = float(Int(propina.value))
+        let propina = cuenta * porcentajeP / 100.0
+        
+        
+        
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
